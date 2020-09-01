34f69d91bc48bb70379823cb8b6446123e62af73
Diego Medina <com1pac56@hotmail.com>
2020-09-01 14:00:55 -0500

    DataFrameImputer added

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,4 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.base import TransformerMixin
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
@@ -14,14 +15,24 @@ class DropColumns(BaseEstimator, TransformerMixin):
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
 
+class DataFrameImputer(TransformerMixin):
 
-class DropNA(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        """Impute missing values.
+
+        Columns of dtype object are imputed with the most frequent value 
+        in column.
+
+        Columns of other types are imputed with mean of column.
+
+        """
     def fit(self, X, y=None):
+
+        self.fill = pd.Series([X[c].value_counts().index[0]
+            if X[c].dtype == np.dtype('O') else X[c].mean() for c in X],
+            index=X.columns)
+
         return self
 
-    def transform(self, X):
-        # Primeiro realizamos a cópia do dataframe 'X' de entrada
-        data = X.copy()
-        df = data.dropna()
-        # Retornamos um novo dataframe sem as colunas indesejadas
-        return df
+    def transform(self, X, y=None):
+        return X.fillna(self.fill)
